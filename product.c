2935343cf8a2e01ecd6e12953627ef60ea091857
Peter Tepela <kerenketepela@gmail.com>
2021-06-22 10:30:26 +0300

    product calculation program

---
+++ product.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int num1, num2;
+    float result;
+
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+
+    result = num1 + num2;
+    printf("Sum: %d %d = %f\n", num1,num2,result );
+
+    return 0;
+}
