7a3008930e73aa0c62b9364309b1af5021f6a93d
Peter Tepela <kerenketepela@gmail.com>
2021-07-21 14:12:14 +0300

    added C loops files

---
+++ C/dowhileloop.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int a = 1;
+    do{
+        printf("hello\n");
+        a++;
+    }while(a<10);
+    return 0;
+}
---
+++ C/whileloop.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int i = 0;
+    while(i<=5){
+        printf("numbers: %d\n", i);
+        i++;
+    }
+    return 0;
+}
