4f21e3a8e8d00c16122c0ae9b7b083f43dbd1654
Peter Tepela <kerenketepela@gmail.com>
2021-07-23 10:32:30 +0300

    assignmet two, added two files

---
+++ C/Assignment2/conversion.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    float distance_in_metres;
+    float convert_to_kilometres;
+
+    printf("Enter distance in meters: ");
+    scanf("%f", &distance_in_metres);
+
+    convert_to_kilometres = distance_in_metres * 0.001;
+
+    printf("converted to: " "%f" "KM", convert_to_kilometres);
+
+    return 0;
+}
---
+++ C/Assignment2/findingthelargest.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+	int first_num;
+	int second_num;
+	int third_num;
+	
+	printf("Enter Three Intergers: ");
+	scanf("%d", &first_num);
+	printf("Enter second Interger: ");
+	scanf("%d", &second_num);
+	printf("Enter a third Interger: ");
+	scanf("%d", &third_num);
+	
+	//logic goes here
+	
+	if(first_num > second_num && first_num > third_num){
+		printf("%d is greter than Both Numbers", first_num);
+	}else if(second_num > first_num && second_num > third_num){
+		printf("%d is greter than Both Numbers", second_num);
+	}else if(third_num > first_num && third_num > second_num)
+	{
+		printf("%d is greter than Both Numbers", third_num);
+	}else
+	{
+		printf("The Numbers Are Equal!!!");
+	}
+	
+	return 0;
+}
